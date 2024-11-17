e4cec46b431942c426e6c00a4ce3c8bd0fc45be2
stakburgers <stakburgers@gmail.com>
2024-11-18 03:37:24 +1300

    Update Blogs.tsx

--- src/pages/Blogs.tsx
+++ src/pages/Blogs.tsx
@@ -9,18 +9,23 @@ const Blogs: React.FC = () => {
   const sortedBlogs = [...mockBlogs].sort((a, b) => b.id - a.id);
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-      {/* Map over the sorted blogs and render BlogCard for each */}
-      {sortedBlogs.map(blog => (
-        <BlogCard
-          key={blog.id}
-          title={blog.title}
-          image={blog.image}
-          description={blog.description}
-          date={blog.date}
-          id={blog.id}
-        />
-      ))}
+    <div className="p-4"> {/* Optional padding for spacing */}
+      {/* Main Title for the Blog Page */}
+      <h1 className="text-5xl text-center mb-7">Blog Posts</h1> {/* Adjust as needed */}
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {/* Map over the sorted blogs and render BlogCard for each */}
+        {sortedBlogs.map(blog => (
+          <BlogCard
+            key={blog.id}
+            title={blog.title}
+            image={blog.image}
+            description={blog.description}
+            date={blog.date}
+            id={blog.id}
+          />
+        ))}
+      </div>
     </div>
   );
 };
